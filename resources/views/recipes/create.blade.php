f9017d42d388b786f17bf5c201dc6524eee93797
black-code1 <legrandmahamat1@gmail.com>
2022-03-28 09:23:03 +0100

    Creation du formulaire pour creer une nouvelle recette

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -7,7 +7,29 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            <div class="mb-5">Creer votre nouvelle recette!</div>
+            <form action="{{ route('recipes.store') }}" method="post">
+                @csrf
+        
+                <x-label value="Titre de la recette" for="title"></x-label>
+                <x-input name="Title" for="title"></x-input>
+        
+                @foreach (App\Models\Ingredient::all() as $ingredient)
+                <div class="my-5" x-data="{isEnabled: false}">
+                    <x-label value="{{ $ingredient->name }}" for="{{ $ingredient->id }}"></x-label>
+                    <x-input type="checkbox" id="{{ $ingredient->id }}" x-model="isEnabled"></x-input>
+        
+                    <x-input name="ingredients[{{ $ingredient->id }}][amount]" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" x-bind:disabled="!isEnabled"></x-input>
+                    <x-input name="ingredients[{{ $ingredient->id }}][unit]" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" x-bind:disabled="!isEnabled"></x-input>
+        
+        
+                </div>
+                    
+                @endforeach
+        
+                <x-button type="submit">Creer ma recette</x-button>
+        
+            </form>
         </div>
     </div>
 </x-app-layout>
